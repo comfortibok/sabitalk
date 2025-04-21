c69560dc0f0a29de3ab0b7dd27882ff3f653a983
Timmydrax <timmydrax@gmail.com>
2025-04-21 11:36:38 +0100

    STYLE: Tried styling icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Router, Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import Onboarding from "./pages/Onboarding";
 import Lesson from "./components/Lesson";
+import ConfirmOtp from "./components/ConfirmOtp.jsx";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Onboarding />} />
         <Route path="/lesson" element={<Lesson />} />
         <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/confirm" element={<ConfirmOtp/>} />
       </Routes>
     </>
   );
