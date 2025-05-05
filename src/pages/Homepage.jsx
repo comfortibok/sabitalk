9f7175138c4921386efa7741729e51520718fd16
Timmydrax <timmydrax@gmail.com>
2025-05-05 18:37:34 +0100

    CHORE: Added Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,31 +1,109 @@
 import styles from "../styles/homepage.module.css";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/images/brand-logo.svg";
+import heroImage from "../assets/images/HeroImage.svg";
+import footerLogo from "../assets/images/footerLogo.svg";
 
 const Homepage = () => {
+  const aboutSection = useRef(null);
+
+  const scrollToSection = (ref) => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <>
-      <div className={styles.wrapper}>
-        <header className={styles.header}>
-          <div className={styles.logo}>
-            <img src="" alt="" />
-          </div>
+    <div className={styles.wrapper}>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <img src={logo} alt="The brand logo" />
+        </div>
 
-          <nav className={styles.nav} >
-            <ul>
-              <li>
-                <Link to="/about">About us</Link>
-              </li>
-            </ul>
-          </nav>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link
+                className={styles.navBtn}
+                to="#about"
+                onClick={() => scrollToSection(aboutSection)}
+              >
+                About us
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-          <div className={styles.ctaBtns}>
-            <a href="">Log in</a>
-            <a href="">Sign Up</a>
+        <div className={styles.ctaBtns}>
+          <Link className={styles.secondaryBtn} to="/login">
+            Log in
+          </Link>
+          <Link className={styles.primaryBtn} to="/sign-up">
+            Sign Up
+          </Link>
+        </div>
+      </header>
+
+      <main>
+        <section className={styles.heroSection}>
+          <div className={styles.leftHero}>
+            <div className={styles.textContainer}>
+              <h1 className={styles.heroText}>
+                Speak Your Culture. Learn Nigerian Languages with Ease
+              </h1>
+              <p className={styles.heroSubtext}>
+                From everyday phrases to deep cultural expressions, master
+                Nigerian languages like Yoruba, Igbo, and Hausa{" "}
+              </p>
+            </div>
+            <Link className={styles.heroBtn} to="/sign-up">
+              Sign Up
+            </Link>
           </div>
-        </header>
-        <section className={styles.heroSection}></section>
-      </div>
-    </>
+          <div className={styles.rightHero}>
+            <img
+              src={heroImage}
+              alt="An Illustration of a tongue showcasing other laguages "
+            />
+          </div>
+        </section>
+
+        <section className={styles.aboutSection} id="about" ref={aboutSection}>
+          <h2 className={styles.aboutHeading}>About Us</h2>
+          <p className={styles.aboutText}>
+            At Speakit, we believe language is more than words—it&#39;s
+            identity, culture, and connection. With fewer young people speaking
+            Nigeria&#39;s native languages, we created a fun, accessible way to
+            learn and love Yoruba, Igbo, Hausa, and more—anytime, anywhere. Our
+            mission is to make Nigerian languages easy to learn, engaging to
+            practice, and cool to speak again. Whether you're a heritage
+            speaker, a curious learner, or a parent teaching the next
+            generation, Speakit is for you. We&#39;re passionate educators,
+            designers, and cultural advocates working to build a future where
+            our languages thrive—in schools, homes, and everyday life.
+          </p>
+        </section>
+      </main>
+
+      <footer className={styles.footer}>
+        <div>
+          <img src={footerLogo} alt="A coloured brand Logo for the footer" />
+        </div>
+        <Link to="#about" onClick={() => scrollToSection(aboutSection)}>
+          About Us
+        </Link>
+        <div className={styles.inputContainer}>
+          <label htmlFor="emailInput">
+            <input
+              type="email"
+              placeholder="Enter your email to sign up"
+              id="emailInput"
+              name="emailInput"
+            />
+          </label>
+          <button className={styles.heroBtn}>Submit</button>
+        </div>
+      </footer>
+    </div>
   );
 };
 
