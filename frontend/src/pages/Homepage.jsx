b325298c12e372b8efa60bfe441f39e229411253
Timmydrax <timmydrax@gmail.com>
2025-05-07 16:31:07 +0100

    CHORE: Added adjustments

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/brand-logo.svg";
 import heroImage from "../assets/images/HeroImage.svg";
-import footerLogo from "../assets/images/footerLogo.svg";
+import footerLogo from "../assets/images/footerLogos.svg";
 
 const Homepage = () => {
   const aboutSection = useRef(null);
